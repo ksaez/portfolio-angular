f361c8705fc1ed16c734c6593fe5228e9cd1da59
Ubuntu <saezgervazzikarol@gmail.com>
2019-03-29 23:04:44 -0300

    agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from "./pages/about/about.component";
 import { ItemComponent } from "./pages/item/item.component";
 
 const app_routes : Routes  = [
-    {path: '', component: PortfolioComponent},
+    {path: 'home', component: PortfolioComponent},
     {path: 'about', component:AboutComponent},
     {path: 'item', component: ItemComponent},
     {path: '**', pathMatch: 'full', redirectTo : ''}
@@ -13,7 +13,7 @@ const app_routes : Routes  = [
 
 @NgModule({
     imports:[
-            RouterModule.forRoot(app_routes)
+            RouterModule.forRoot(app_routes, { useHash: true })
     ],
     exports: [
         RouterModule
